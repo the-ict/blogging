3600f3ef836ae97e40f11cb36f4608977a010291
the-ict <dvltinv@gmail.com>
2025-07-29 09:29:37 +0500

    fixed old errors

--- routers/post.js
+++ routers/post.js
@@ -6,6 +6,8 @@ router.post("/", async (req, res) => {
     try {
         const newPost = new Post(req.body)
         const savedPost = await newPost.save()
+
+        console.log(savedPost)
         res.status(200).json(savedPost)
     } catch (error) {
         res.status(500).json(error)
